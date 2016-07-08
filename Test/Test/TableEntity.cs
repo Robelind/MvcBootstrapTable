71af397516b306878b6e664f342b05cf8541ed42
Robert Lindblom <robert.lindblom@live.se>
2016-07-08 11:37:48 +0200

    -

---
+++ Test/Test/TableEntity.cs
@@ -0,0 +1,9 @@
+namespace Test
+{
+    public class TableEntity
+    {
+        public string Property { get; set; }
+        public string Property2 { get; set; }
+        public int Property3 { get; set; }
+    }
+}
